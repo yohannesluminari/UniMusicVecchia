b79ac1c59a217cd7bb27a7c45b8ea539245f3bf1
yohannesluminari <y.luminari@gmail.com>
2024-06-09 15:45:44 +0200

    nav

--- UniMusic/src/app/components/header/navbar/navbar.component.ts
+++ UniMusic/src/app/components/header/navbar/navbar.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
 
-  show:boolean = false
+  constructor(private router: Router) {}
+
   showNavbar: boolean = false;
 
   toggleNavbar(): void {
     this.showNavbar = !this.showNavbar;
+  }
+
+  isLinkActive(route: string): boolean {
+    return this.router.url === route;
   }
 }
