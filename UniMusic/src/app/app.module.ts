168dc1e156e6c762d247f939b36c340d99eb1f53
yohannesluminari <y.luminari@gmail.com>
2024-06-13 16:52:54 +0200

    carousel

--- UniMusic/src/app/app.module.ts
+++ UniMusic/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { SwitchComponent } from './components/switch/switch.component';
 import { NavbarComponent } from './components/header/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SwitchComponent,
     NavbarComponent
   ],
   imports: [
