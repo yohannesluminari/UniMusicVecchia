e3e30507c03af88f29746fdfda438df442ac0af2
yohannesluminari <y.luminari@gmail.com>
2024-06-13 15:35:18 +0200

    CAROUSEL

--- UniMusic/src/app/components/carousel/carousel.component.ts
+++ UniMusic/src/app/components/carousel/carousel.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.scss'
+  styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent {
-
+  images = [
+    { src: '/assets/artistiImg/1.jpg', alt: 'Artista 1' },
+    { src: '/assets/artistiImg/2.jpg', alt: 'Artista 2' },
+    { src: '/assets/artistiImg/3.jpg', alt: 'Artista 3' },
+    // Add more images as needed
+  ];
 }
