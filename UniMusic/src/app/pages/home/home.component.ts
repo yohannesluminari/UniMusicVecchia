62f37b8405e477a5b92dc625ffb0951d4bcaf8df
yohannesluminari <y.luminari@gmail.com>
2024-07-02 16:25:02 +0200

    non ricordo

--- UniMusic/src/app/pages/home/home.component.ts
+++ UniMusic/src/app/pages/home/home.component.ts
@@ -25,13 +25,12 @@ export class HomeComponent {
   toggleAudio(index: number): void {
     // Verifica se lo stesso cubo è stato premuto di nuovo senza premere altri cubi prima
     if (this.currentAudioIndex === index) {
-      // Se sì, interrompi la riproduzione dell'audio corrente
       this.audioService.pause();
-      this.currentAudioIndex = null; // Resetta l'indice dell'audio corrente
+      this.currentAudioIndex = null; 
     } else {
       const audioSrc = `./assets/${this.audioFiles[index]}`;
 
-      // Avvia la riproduzione dell'audio desiderato
+
       this.audioService.play(audioSrc);
       this.currentAudioIndex = index; // Salva l'indice dell'audio corrente
     }
