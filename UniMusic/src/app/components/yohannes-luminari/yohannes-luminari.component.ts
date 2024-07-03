61bf13f14d7bdb1b5cda574fd376a8ced277dd21
yohannesluminari <y.luminari@gmail.com>
2024-07-03 14:57:21 +0200

    profile

--- UniMusic/src/app/components/yohannes-luminari/yohannes-luminari.component.ts
+++ UniMusic/src/app/components/yohannes-luminari/yohannes-luminari.component.ts
@@ -6,14 +6,21 @@ import { Component } from '@angular/core';
   styleUrl: './yohannes-luminari.component.scss'
 })
 export class YohannesLuminariComponent {
-  roles: string[] = ['Full Stack Developer', 'Software Engineer', 'Creative Coder', 'Tech Enthusiast'];
+  roles: string[] = ['Full Stack Developer', 'Front-end Developer', 'Back-end Developer'];
   currentRole: string = this.roles[0];
   currentIndex: number = 0;
 
+  animatedArtists: number = 0;
+  animatedTracks: number = 0;
+
+  private maxArtists: number = 188;
+  private maxTracks: number = 600;
+  
   constructor() { }
 
   ngOnInit(): void {
     this.startRoleRotation();
+    this.animateNumbers();
   }
 
   startRoleRotation() {
@@ -23,7 +30,6 @@ export class YohannesLuminariComponent {
     }, 3000); // Cambia ogni 3 secondi
   }
 
-
   scrollToContact() {
     const contactSection = document.getElementById('contact-section');
     if (contactSection) {
@@ -32,4 +38,28 @@ export class YohannesLuminariComponent {
     }
   }
 
+  animateNumbers() {
+    const duration = 2000; // Durata dell'animazione in millisecondi
+    const steps = 60; // Numero di passaggi nell'animazione
+    const artistIncrement = this.maxArtists / steps;
+    const trackIncrement = this.maxTracks / steps;
+
+    let currentArtist = 0;
+    let currentTrack = 0;
+    let stepCount = 0;
+
+    const interval = setInterval(() => {
+      if (stepCount >= steps) {
+        clearInterval(interval);
+        this.animatedArtists = this.maxArtists;
+        this.animatedTracks = this.maxTracks;
+      } else {
+        currentArtist += artistIncrement;
+        currentTrack += trackIncrement;
+        this.animatedArtists = Math.floor(currentArtist);
+        this.animatedTracks = Math.floor(currentTrack);
+        stepCount++;
+      }
+    }, duration / steps);
+  }
 }
